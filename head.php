8f07947d339115a109c336e413e3dd99a68a9b48
Fabian Justo <fabianjusto2341@gmail.com>
2022-05-13 08:37:44 -0400

    13-05

--- head.php
+++ head.php
@@ -2,6 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="styles/lightstyle.css" id="css-theme">
+<link rel="stylesheet" href="styles/style.css">
+<link rel="stylesheet" href="styles/light.css" id="css-theme">
 <link rel="stylesheet" href="styles/header-style.css">
 <script src="switchTheme.js"></script>
